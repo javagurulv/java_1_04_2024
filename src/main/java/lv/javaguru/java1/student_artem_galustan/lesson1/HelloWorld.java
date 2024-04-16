48bbf3c3cc3bbbc2dbd61d3b4ccc5bd756faf275
TheBigG1 <galustjans.artjoms@gmail.com>
2024-04-16 21:11:04 +0300

    HelloWorld example

---
+++ src/main/java/lv/javaguru/java1/student_artem_galustan/lesson1/HelloWorld.java
@@ -0,0 +1,14 @@
+package lv.javaguru.java1.student_artem_galustan.lesson1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Artem");
+
+
+        System.out.println("Galustan");
+
+    }
+
+
+
+}

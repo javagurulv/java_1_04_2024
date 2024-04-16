b30853ddfe9c319a0ce318b73dd6962741c7e42c
TheMaksimys <maksims.latkovskis@gmail.com>
2024-04-16 21:11:37 +0300

    Created first program

---
+++ src/main/java/lv/javaguru/java1/student_maksims_latkovskis/lesson_1/HelloWorld.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_maksims_latkovskis.lesson_1;
+
+public class HelloWorld
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Maksims");
+        System.out.println("Latkovskis");
+    }
+
+}

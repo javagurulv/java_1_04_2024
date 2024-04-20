0048bb65d843a83c0e693544cd13ef4cfc6aec59
Pahanec <87240194+Pahanec@users.noreply.github.com>
2024-04-20 17:18:42 +0300

    Task 5 and 6 for lesson 1

---
+++ src/main/java/lv/javaguru/java1/student_pavels_zuriha/lesson_1/Level_6.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_pavels_zuriha.lesson_1;
+
+public class Level_6 {
+    public static void main(String[] args) {
+        System.out.println(74+36);
+        System.out.println(50/3);
+    }
+}

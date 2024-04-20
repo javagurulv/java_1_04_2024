ced19b3dce736b9aeb05aef45776d04a310e17c5
NSnadezhda <nadezda.senko@gmail.com>
2024-04-20 19:37:42 +0300

    Level5Task1

---
+++ src/main/java/lv/javaguru/java1/student_nadezda_radigina/lesson1/level_5/Task1.java
@@ -0,0 +1,7 @@
+package lv.javaguru.java1.student_nadezda_radigina.lesson1.level_5;
+
+public class Task1 {
+    public static void main (String[] args){
+        System.out.println("Hello World!");
+    }
+}

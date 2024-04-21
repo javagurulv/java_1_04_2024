723ec1b84d04c1b75b44ccb07cc438116c54b120
stemcenko <67960125+stemcenko@users.noreply.github.com>
2024-04-21 09:31:03 +0300

    Lesson_1 Task_1: выведет на консоль результат умножения двух чисел

---
+++ src/main/java/lv/javaguru/java1/student_sergejs_temcenko/lesson_1/level_6_middle/task_2/Task_2.java
@@ -0,0 +1,7 @@
+package lv.javaguru.java1.student_sergejs_temcenko.lesson_1.level_6_middle.task_2;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        System.out.println(20 * 4);
+    }
+}

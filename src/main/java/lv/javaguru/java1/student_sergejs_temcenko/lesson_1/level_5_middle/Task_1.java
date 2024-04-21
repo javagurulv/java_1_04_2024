a9ee14d0a368e18ad5f24f256bbaa3c8f96324e2
stemcenko <67960125+stemcenko@users.noreply.github.com>
2024-04-21 09:46:30 +0300

    Lesson_1 Level_5 Task_1: увидите текст "Hello World!"

---
+++ src/main/java/lv/javaguru/java1/student_sergejs_temcenko/lesson_1/level_5_middle/Task_1.java
@@ -0,0 +1,7 @@
+package lv.javaguru.java1.student_sergejs_temcenko.lesson_1.level_5_middle;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

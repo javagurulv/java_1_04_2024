5c0b40702d4135f1dce02879c74670b06adbeb33
TheBigG1 <galustjans.artjoms@gmail.com>
2024-04-23 20:16:00 +0300

    Lesson 3

---
+++ src/main/java/lv/javaguru/java1/student_artem_galustan/lesson1/Calculators.java
@@ -0,0 +1,24 @@
+package lv.javaguru.java1.student_artem_galustan.lesson1;
+
+public class Calculators {
+    public static void main(String[] args) {
+
+        int number1 = 200;
+        int number2 = 50;
+        int number3 = 60;
+        int number4 = 200;
+
+        int sum = number1 + number2;
+        int multiply = number3 * number4;
+        int divide = number1 / number2;
+        int substract = number1 - number2;
+
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+
+
+    }
+}

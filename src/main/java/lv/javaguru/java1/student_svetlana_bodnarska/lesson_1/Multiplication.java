de5da2293966c6f3f34f66f9e8b6c1295536ed1f
S286B826 <svetlana.bodnarska@inbox.lv>
2024-04-23 16:53:25 +0300

    Lesson_1, Level_6, Task_2

---
+++ src/main/java/lv/javaguru/java1/student_svetlana_bodnarska/lesson_1/Multiplication.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_svetlana_bodnarska.lesson_1;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int n=50;
+        int k=3;
+        int m=n*k;
+        System.out.println (m);
+
+    }
+}

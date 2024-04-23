4cc78710a7285259a2cba5e22809d34a4f200f0f
amezateru <amezateru@gmail.com>
2024-04-23 22:21:11 +0300

    Lesson3

--- src/main/java/lv/javaguru/java1/student_nikita_paramonovs/lesson_2/QuadraticEquationSolver.java
+++ src/main/java/lv/javaguru/java1/student_nikita_paramonovs/lesson_2/QuadraticEquationSolver.java
@@ -4,10 +4,7 @@ import java.util.Scanner;
 
 public class QuadraticEquationSolver {
     public static void main(String[] args){
-
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter (ax^2 + bx + c = 0):");
 
         System.out.print("Enter a: ");
         double a = scanner.nextDouble();

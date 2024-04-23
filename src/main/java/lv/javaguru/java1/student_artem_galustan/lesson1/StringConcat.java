1073c509a96db456fac6dd097032d950ddf068d3
TheBigG1 <galustjans.artjoms@gmail.com>
2024-04-23 20:55:42 +0300

    Concat

---
+++ src/main/java/lv/javaguru/java1/student_artem_galustan/lesson1/StringConcat.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_artem_galustan.lesson1;
+
+public class StringConcat {
+    public static void main(String[] args) {
+
+        String firstname = "Vasja ";
+        String lastname = "Papkin";
+                String fullname = firstname + lastname;
+    }
+}

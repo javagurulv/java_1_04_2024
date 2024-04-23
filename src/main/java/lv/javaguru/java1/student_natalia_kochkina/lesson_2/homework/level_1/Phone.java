1e652c5973c794d734cbdbd3e9b3adb7e9eeda3c
Natalia Kochkina <tisha.silence12@gmail.com>
2024-04-23 23:45:26 +0300

    lesson2 homework lvl 1,2,3

---
+++ src/main/java/lv/javaguru/java1/student_natalia_kochkina/lesson_2/homework/level_1/Phone.java
@@ -0,0 +1,5 @@
+package lv.javaguru.java1.student_natalia_kochkina.lesson_2.homework.level_1;
+
+public class Phone {
+    //Task_2
+}
---
+++ src/main/java/lv/javaguru/java1/student_natalia_kochkina/lesson_2/homework/level_3/AverageNumber.java
@@ -0,0 +1,26 @@
+package lv.javaguru.java1.student_natalia_kochkina.lesson_2.homework.level_3;
+
+import java.util.Scanner;
+
+public class AverageNumber {
+
+    //Task_3
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number 1: ");
+        int number1 = scanner.nextInt();
+
+        System.out.println("Enter number 2: ");
+        int number2 = scanner.nextInt();
+
+        System.out.println("Enter number 3: ");
+        int number3 = scanner.nextInt();
+
+        double average = (number1 + number2 + number3) / ((double) 3);
+
+        System.out.println("average = " + average);
+
+    }
+}

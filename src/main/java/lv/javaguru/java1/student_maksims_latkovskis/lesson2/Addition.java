8ddc81acf1ea610e3757d6b20178626e19d357ec
TheMaksimys <maksims.latkovskis@gmail.com>
2024-04-23 19:21:16 +0300

    Created first program

---
+++ src/main/java/lv/javaguru/java1/student_maksims_latkovskis/lesson2/Addition.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java1.student_maksims_latkovskis.lesson2;
+
+
+public class Addition {
+
+    public static void main(String[] args){
+
+        int Number1=5;
+        int Number2=10;
+        long Sum=Number1+Number2;
+        System.out.println(Sum);
+
+    }
+
+
+}

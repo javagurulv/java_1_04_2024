8d163414a75dcb3044c2962cd848401e5c69ef6f
TheMaksimys <maksims.latkovskis@gmail.com>
2024-04-23 20:54:33 +0300

    Created first program

---
+++ src/main/java/lv/javaguru/java1/student_maksims_latkovskis/lesson2/GlueLines.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java1.student_maksims_latkovskis.lesson2;
+
+public class GlueLines {
+
+    public static void main(String[] args){
+
+        String FirstName = "Maksims";
+        String LastName = "Latkovskis";
+        String FullName = FirstName + " " + LastName;
+
+        System.out.println(FullName);
+
+    }
+
+
+}

370d1458af5b7f755813d08f51ba62bf7cbdcdf9
S286B826 <svetlana.bodnarska@inbox.lv>
2024-04-23 16:29:05 +0300

    Sum example (74+36+110)

---
+++ src/main/java/lv/javaguru/java1/student_svetlana_bodnarska/lesson_1/Sum.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_svetlana_bodnarska.lesson_1;
+
+public class Sum {
+    public static void main(String[] args) {
+       int n=74;
+       int k=36;
+       int m=n+k;
+       System.out.println (m);
+
+    }
+}

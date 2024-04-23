5acf54254a06009fa8ffb5542ae55cc4050ffb17
javagurulv <javagurulv@gmail.com>
2024-04-23 21:22:29 +0300

    LESSON 2 triangle square

--- src/main/java/lv/javaguru/java1/teacher/lesson_2_variables/lessoncode/TriangleSquare.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_2_variables/lessoncode/TriangleSquare.java
@@ -8,11 +8,20 @@ public class TriangleSquare {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter number: ");
-        int number1 = scanner.nextInt();
+        System.out.println("Enter a: ");
+        int a = scanner.nextInt();
 
+        System.out.println("Enter b: ");
+        int b = scanner.nextInt();
 
-        System.out.println(number1);
+        System.out.println("Enter c: ");
+        int c = scanner.nextInt();
+
+        double p = (a + b + c) / (double) 2;
+
+        double square = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+        System.out.println("Triangle square = " + square);
 
     }
 

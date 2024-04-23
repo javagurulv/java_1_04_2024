8d0430075f62d8c48dc808d37f9cb8e873099906
Natalia Kochkina <tisha.silence12@gmail.com>
2024-04-23 20:05:29 +0300

    calculator_lesson2

---
+++ src/main/java/lv/javaguru/java1/student_natalia_kochkina/lesson_2/Calculator.java
@@ -0,0 +1,25 @@
+package lv.javaguru.java1.student_natalia_kochkina.lesson_2;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+
+        int number1 = 100;
+
+        int number2 = 200;
+
+        int additionResult = number1 + number2;
+
+        int subtractionResult = number1 - number2;
+
+        int multiplicationResult = number1 * number2;
+
+        int divisionResult = number1 / number2;
+
+        System.out.println("additionResult = " + additionResult);
+        System.out.println("subtractionResult = " + subtractionResult);
+        System.out.println("multiplicationResult = " + multiplicationResult);
+        System.out.println("divisionResult = " + divisionResult);
+    }
+}

06591c89b9985f9681610b4a6609a277e328b097
TheMaksimys <maksims.latkovskis@gmail.com>
2024-04-23 20:27:22 +0300

    Created first program

---
+++ src/main/java/lv/javaguru/java1/student_maksims_latkovskis/lesson2/ArithmeticsReal.java
@@ -0,0 +1,20 @@
+package lv.javaguru.java1.student_maksims_latkovskis.lesson2;
+
+public class ArithmeticsReal {
+
+        public static void main(String[] args) {
+
+            float Number1 = 50;
+            int Number2 = 3;
+            float Sum = Number1 + Number2;
+            float Diff = Number1 - Number2;
+            float Mux = Number1 * Number2;
+            float Div = Number1 / Number2;
+
+            System.out.println("The results are follows:");
+            System.out.println("Sum:" + Sum + " Difference:" + Diff + " Multiplication:" + Mux + " Division:" + Div);
+
+        }
+
+
+}

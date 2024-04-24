a745415b74e0fa65002f19239303036104fad35e
Natalia Kochkina <tisha.silence12@gmail.com>
2024-04-24 23:23:03 +0300

    lesson2 homework lvl 4,5,6,7

---
+++ src/main/java/lv/javaguru/java1/student_natalia_kochkina/lesson_2/homework/level_7/NoSleepNoFocus.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_natalia_kochkina.lesson_2.homework.level_7;
+
+public class NoSleepNoFocus {
+
+    //Task_1
+
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}

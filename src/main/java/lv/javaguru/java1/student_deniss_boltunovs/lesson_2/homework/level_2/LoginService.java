70ae92ffd90f02167b653a3f1f3cab549a4e3165
Den <deniss.boltunovs@gmail.com>
2024-04-24 10:40:06 +0300

    Lesson 2 homework level_2 LoginService

---
+++ src/main/java/lv/javaguru/java1/student_deniss_boltunovs/lesson_2/homework/level_2/LoginService.java
@@ -0,0 +1,17 @@
+package lv.javaguru.java1.student_deniss_boltunovs.lesson_2.homework.level_2;
+
+/*
+class LoginService {
+
+	public void main(String[] args) {
+		System.out.println("You can login!");
+	}
+}
+ */
+
+public class LoginService {
+
+     public static void main(String[] args) {
+         System.out.println("You can login!");
+     }
+}

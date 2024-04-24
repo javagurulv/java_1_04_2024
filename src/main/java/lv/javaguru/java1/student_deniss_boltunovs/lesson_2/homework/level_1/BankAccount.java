04f4379ab6eb4b04129af01769d603ba6875a532
Den <deniss.boltunovs@gmail.com>
2024-04-24 10:13:41 +0300

    Lesson 2 homework level_1 BankAccount

---
+++ src/main/java/lv/javaguru/java1/student_deniss_boltunovs/lesson_2/homework/level_1/BankAccount.java
@@ -0,0 +1,18 @@
+package lv.javaguru.java1.student_deniss_boltunovs.lesson_2.homework.level_1;
+
+/*
+class BankAccount {
+
+	public static void main(String[] args) {
+
+}
+ */
+
+public class BankAccount {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}

bd57512f8617e75b22da692d377943627ade095b
Den <deniss.boltunovs@gmail.com>
2024-04-24 15:31:06 +0300

    Lesson 2 homework weekly_challanges Task 3

---
+++ src/main/java/lv/javaguru/java1/student_deniss_boltunovs/lesson_2/homework/weekly_challanges/task_3/CorrectDataTypes.java
@@ -0,0 +1,38 @@
+package lv.javaguru.java1.student_deniss_boltunovs.lesson_2.homework.weekly_challanges.task_3;
+
+/*
+Исправьте ошибки компиляции в данной программе.
+
+public class CorrectDataTypes {
+
+	public static void main(String[] args) {
+
+		int numberOne = 10;
+		double numberTwo = 20.0;
+
+		System.out.println("Number 1 = " + numberOne);
+		System.out.println("Number 2 = " + numberTwo);
+
+		float sum = numberOne + numberTwo;
+
+		System.out.println("Sum = " + sum);
+	}
+}
+ */
+
+public class CorrectDataTypes {
+
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        double numberTwo = 20.0;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        double sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+
+    }
+}

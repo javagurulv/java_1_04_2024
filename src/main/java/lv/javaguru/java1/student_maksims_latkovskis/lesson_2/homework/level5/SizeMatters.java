2e64499ee72913d1d1da3ee8ec6c239bc5bb2996
TheMaksimys <maksims.latkovskis@gmail.com>
2024-04-26 16:49:52 +0300

    Fixed typing errors

---
+++ src/main/java/lv/javaguru/java1/student_maksims_latkovskis/lesson_2/homework/level5/SizeMatters.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_maksims_latkovskis.lesson_2.homework.level5;
+
+ class SizeMatters {
+     public static void main(String[] args) {
+         System.out.println("Sorry, I have an errors.");
+         System.out.println("Please fix me!");
+     }
+
+}

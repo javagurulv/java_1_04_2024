31de26cc9d426dc9f2be48912a7cc41ccc60044e
a.eternalstudent <a.eternalstudent@gmail.com>
2024-04-26 19:16:01 +0200

    added Lesson 3 Level 4 Task 1

---
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_3/homework/level_4_junior/Task_1/OneLine.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_anton_pereloma.lesson_3.homework.level_4_junior.Task_1;
+
+public class OneLine {
+    public static void main(String[] args) {
+        System.out.print("I'm a ");
+        System.out.print("one liner text");
+    }
+}

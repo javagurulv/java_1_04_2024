da6974535e4adbc3f9ba5afb1eb7e7f5b07757af
NSnadezhda <nadezda.senko@gmail.com>
2024-04-26 22:41:34 +0300

    Lesson2 Level4

---
+++ src/main/java/lv/javaguru/java1/student_nadezda_radigina/lesson2/level4/Text.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_nadezda_radigina.lesson2.level4;
+
+public class Text {
+    public static void main(String[] args){
+        System.out.println("Text1");
+        //move to next line
+        System.out.print("Text2");
+        //stays on the same line
+        System.out.println("Text3");
+
+    }
+}

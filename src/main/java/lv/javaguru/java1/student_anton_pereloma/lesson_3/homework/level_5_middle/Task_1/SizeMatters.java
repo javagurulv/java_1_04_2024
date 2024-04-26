796a9813c5843aa5685902bcea4b94fadd2e1a35
a.eternalstudent <a.eternalstudent@gmail.com>
2024-04-26 19:23:24 +0200

    completed Lesson 3 Level 5 Task 1

---
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_3/homework/level_5_middle/Task_1/SizeMatters.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_anton_pereloma.lesson_3.homework.level_5_middle.Task_1;
+
+public class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have errors.");
+        System.out.println("Please, fix me!");
+    }
+}
+

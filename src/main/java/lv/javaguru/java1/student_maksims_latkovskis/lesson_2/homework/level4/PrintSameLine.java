e987f8f83b940625b37f44295a476bc163bf2f46
TheMaksimys <maksims.latkovskis@gmail.com>
2024-04-26 15:35:26 +0300

    Created first program

---
+++ src/main/java/lv/javaguru/java1/student_maksims_latkovskis/lesson_2/homework/level4/PrintSameLine.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_maksims_latkovskis.lesson_2.homework.level4;
+
+public class PrintSameLine {
+
+    public static void main(String[] args){
+
+        System.out.print("Java ");
+        System.out.print("GURU");
+
+    }
+
+
+}

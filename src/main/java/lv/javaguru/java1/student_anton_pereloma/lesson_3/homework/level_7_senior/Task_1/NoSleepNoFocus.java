3ce7544b4a226ab1ae92c164a61918d967d9b9c1
a.eternalstudent <a.eternalstudent@gmail.com>
2024-04-26 19:32:53 +0200

    completed Lesson 3 Level 7 Task 1

---
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_3/homework/level_7_senior/Task_1/NoSleepNoFocus.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_anton_pereloma.lesson_3.homework.level_7_senior.Task_1;
+
+class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}

00927a48566de0a2ec90d74ee3b82d76f78512a8
NSnadezhda <nadezda.senko@gmail.com>
2024-04-27 07:27:20 +0300

    Lesson2 Level6

---
+++ src/main/java/lv/javaguru/java1/student_nadezda_radigina/lesson2/level6/AttentionPlease.java
@@ -0,0 +1,7 @@
+package lv.javaguru.java1.student_nadezda_radigina.lesson2.level6;
+
+public class AttentionPlease {
+    public static void main(String[] args){
+        System.out.println("Code outside the method will not compile!");
+    }
+}

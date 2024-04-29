87252cd79e3ac12acafab7d01214deda64fc1a0e
Natalia Kochkina <tisha.silence12@gmail.com>
2024-04-29 16:37:10 +0300

    lesson2 homework weekly challenges

---
+++ src/main/java/lv/javaguru/java1/student_natalia_kochkina/lesson_2/homework/weekly_challenges/SumTwoNumbers.java
@@ -0,0 +1,19 @@
+package lv.javaguru.java1.student_natalia_kochkina.lesson_2.homework.weekly_challenges;
+
+public class SumTwoNumbers {
+
+    //Task_2
+
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        int numberTwo = 20;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        int sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+    }
+}

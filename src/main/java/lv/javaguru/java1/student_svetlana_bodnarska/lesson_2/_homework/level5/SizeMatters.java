bdef4a9ae273d9b7e5e9502c7d672160b09a65a7
S286B826 <svetlana.bodnarska@inbox.lv>
2024-04-30 12:23:50 +0300

    Lesson_5, Task_1

---
+++ src/main/java/lv/javaguru/java1/student_svetlana_bodnarska/lesson_2/_homework/level5/SizeMatters.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_svetlana_bodnarska.lesson_2._homework.level5;
+
+public class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}

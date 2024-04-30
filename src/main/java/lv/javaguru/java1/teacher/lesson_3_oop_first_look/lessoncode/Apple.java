05d407b908e9c14bb2a718be3f31ee1b594b6be0
javagurulv <javagurulv@gmail.com>
2024-04-30 21:19:32 +0300

    LESSON 3

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_3_oop_first_look/lessoncode/Apple.java
@@ -0,0 +1,15 @@
+package lv.javaguru.java1.teacher.lesson_3_oop_first_look.lessoncode;
+
+class Apple {
+
+    private String type;
+    private String color;
+    private int weight;
+
+    public Apple(String type, String color, int weight) {
+        this.type = type;
+        this.color = color;
+        this.weight = weight;
+    }
+
+}

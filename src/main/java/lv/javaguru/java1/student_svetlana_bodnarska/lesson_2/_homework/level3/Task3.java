457c96493ccec60ae03d204a96a6eab88849750d
S286B826 <svetlana.bodnarska@inbox.lv>
2024-04-30 11:00:26 +0300

    Lesson_3, Task_1,2,3

---
+++ src/main/java/lv/javaguru/java1/student_svetlana_bodnarska/lesson_2/_homework/level3/Task3.java
@@ -0,0 +1,17 @@
+package lv.javaguru.java1.student_svetlana_bodnarska.lesson_2._homework.level3;
+
+import java.util.Scanner;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System. in );
+        int a, b, c;
+        System.out.println("Enter numbers");
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        c = scanner.nextInt();
+        System.out.println((a + b + c) / 3);
+
+    }
+}

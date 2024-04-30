903d1266548c2465812327d5f2fbe144172ec594
amezateru <amezateru@gmail.com>
2024-04-30 19:01:42 +0300

    Lesson3

---
+++ src/main/java/lv/javaguru/java1/student_nikita_paramonovs/lesson_2/averages.java
@@ -0,0 +1,20 @@
+package lv.javaguru.java1.student_nikita_paramonovs.lesson_2;
+
+import java.util.Scanner;
+
+public class averages {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter num1: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("enter num2: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("enter num3: ");
+        int num3 = scanner.nextInt();
+        double average = (num1 + num2 + num3) / 3.0;
+        System.out.println("average " + num1 + ", " + num2 + " and " + num3 + " equals " + average);
+    }
+
+}
---
+++ src/main/java/lv/javaguru/java1/student_nikita_paramonovs/lesson_2/homework.java
@@ -0,0 +1,22 @@
+package lv.javaguru.java1.student_nikita_paramonovs.lesson_2;
+
+import java.util.Scanner;
+
+public class homework {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter number: ");
+        int number = scanner.nextInt();
+
+        System.out.println("result" + number + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+
+
+
+    }
+
+
+}

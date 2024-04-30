fa84bf99e5bce37649c657bb6aca6ad089401bd4
S286B826 <svetlana.bodnarska@inbox.lv>
2024-04-30 13:08:04 +0300

    Lesson_6, Task_3

---
+++ src/main/java/lv/javaguru/java1/student_svetlana_bodnarska/lesson_2/_homework/level6/Task3.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_svetlana_bodnarska.lesson_2._homework.level6;
+
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+    }
+
+}

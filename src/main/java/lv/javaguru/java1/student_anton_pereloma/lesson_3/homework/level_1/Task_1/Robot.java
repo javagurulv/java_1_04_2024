130c8f43c92428df9d8f8dd2b69b069c210472ef
a.eternalstudent <a.eternalstudent@gmail.com>
2024-05-01 19:26:29 +0200

    Added lesson 4 notes. Completed lesson 4 level 1 Task_1.

---
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_3/homework/level_1/Task_1/Robot.java
@@ -0,0 +1,18 @@
+package lv.javaguru.java1.student_anton_pereloma.lesson_3.homework.level_1.Task_1;
+
+public class Robot {
+    private String name;
+
+    public Robot(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void changeName(String name) {
+        this.name = name;
+    }
+
+}

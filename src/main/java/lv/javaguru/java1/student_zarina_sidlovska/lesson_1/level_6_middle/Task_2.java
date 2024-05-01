6159df7e7456734cb0453bfc0c8c7b5ef85857ad
zarinajorova <z.jorova@gmail.com>
2024-05-01 18:15:05 +0300

    homework for 2nd lesson - done

---
+++ src/main/java/lv/javaguru/java1/student_zarina_sidlovska/lesson_1/level_6_middle/Task_2.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_zarina_sidlovska.lesson_1.level_6_middle;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        int nr1 = 50;
+        int nr2 = 3;
+        int result = nr1 * nr2;
+
+        System.out.println(result);
+    }
+}

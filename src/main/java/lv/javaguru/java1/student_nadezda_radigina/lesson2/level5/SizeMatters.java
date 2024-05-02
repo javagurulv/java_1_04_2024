4ba43ddda5dcafbaaec079e634f7a625162bc1f4
javagurulv <javagurulv@gmail.com>
2024-05-02 09:42:42 +0300

    code review

--- src/main/java/lv/javaguru/java1/student_nadezda_radigina/lesson2/level5/SizeMatters.java
+++ src/main/java/lv/javaguru/java1/student_nadezda_radigina/lesson2/level5/SizeMatters.java
@@ -1,5 +1,6 @@
 package lv.javaguru.java1.student_nadezda_radigina.lesson2.level5;
 
+//CODE_REVIEW: please write main() method in the new line
 public class SizeMatters {public static void main(String[] args) {
 
     System.out.println("Sorry, I have an errors.");

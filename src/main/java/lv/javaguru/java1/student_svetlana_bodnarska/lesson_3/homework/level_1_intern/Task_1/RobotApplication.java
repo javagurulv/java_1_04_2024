b28962f4cfdb2518ffd9bcd3e297048a7b39bdf3
S286B826 <svetlana.bodnarska@inbox.lv>
2024-05-03 23:08:40 +0300

    Level_1

---
+++ src/main/java/lv/javaguru/java1/student_svetlana_bodnarska/lesson_3/homework/level_1_intern/Task_1/RobotApplication.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_svetlana_bodnarska.lesson_3.homework.level_1_intern.Task_1;
+
+class RobotApplication {
+    public static void main(String[] args) {
+        Robot robot1 = new Robot ("Zora");
+
+    }
+}

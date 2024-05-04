58c91a913c8dcc1269d1237da0647b2166898a3b
a.eternalstudent <a.eternalstudent@gmail.com>
2024-05-04 12:01:48 +0200

    Completed homework lesson 3 level 5 task 2

---
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_3/homework/level_5/Phone.java
@@ -0,0 +1,25 @@
+package lv.javaguru.java1.student_anton_pereloma.lesson_3.homework.level_5;
+
+class Phone {
+
+    private String model;
+
+    public Phone(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+}
+
+class PhoneDemo {
+
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+
+}

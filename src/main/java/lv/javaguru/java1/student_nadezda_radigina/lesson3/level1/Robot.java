8b527d1f9cbf194f243fe932d4f192b4f308684d
NSnadezhda <nadezda.senko@gmail.com>
2024-05-04 15:58:40 +0300

    Lesson3 level1

---
+++ src/main/java/lv/javaguru/java1/student_nadezda_radigina/lesson3/level1/Robot.java
@@ -0,0 +1,17 @@
+package lv.javaguru.java1.student_nadezda_radigina.lesson3.level1;
+
+ class Robot {
+     private String name;
+     public Robot(String name){
+         this.name = name;
+     }
+     public String getName() {
+         return this.name;
+     }
+         public void setName(String name){
+             this.name = name;
+         }
+
+
+}
+

4918ffaf24fa624f02263b808712dbe02abee87b
a.eternalstudent <a.eternalstudent@gmail.com>
2024-05-04 14:52:25 +0200

    Completed homework lesson 3 level 7 task 3

---
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_3/homework/level_7_senior/Dog.java
@@ -0,0 +1,26 @@
+package lv.javaguru.java1.student_anton_pereloma.lesson_3.homework.level_7_senior;
+
+class Dog {
+    private String name;
+    private int age;
+
+    Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    String sayHello() {
+        return "My name is " + name;
+    }
+
+    String sayAge() {
+        if (this.age != 1) {
+            return "I'm " + this.age + " years old.";
+        } else {
+            return "I'm " + this.age + " year old.";
+        }
+    }
+
+    public void happyBirthday() {
+        this.age ++;
+    }
+}

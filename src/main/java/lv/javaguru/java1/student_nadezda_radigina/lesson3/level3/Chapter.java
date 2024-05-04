00cfaeb588ef7b82a50671301d41514733c5f7f9
NSnadezhda <nadezda.senko@gmail.com>
2024-05-04 18:11:07 +0300

    Lesson3 level3

---
+++ src/main/java/lv/javaguru/java1/student_nadezda_radigina/lesson3/level3/Chapter.java
@@ -0,0 +1,17 @@
+package lv.javaguru.java1.student_nadezda_radigina.lesson3.level3;
+
+public class Chapter {
+    String name;
+    String text;
+    public Chapter(String name, String text){
+        this.name = name;
+        this.text = text;
+    }
+
+    public String getName(){
+        return this.name = name;
+    }
+    public String getText(){
+        return this.text = text;
+    }
+}

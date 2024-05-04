f82c9827eb8e25f5ffeff43cc93aa4e6488a9374
a.eternalstudent <a.eternalstudent@gmail.com>
2024-05-04 12:55:25 +0200

    Completed homework lesson 3 level 7 task 1

--- src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_3/homework/level_7_senior/Circle.java
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_3/homework/level_7_senior/Circle.java
@@ -1,4 +1,18 @@
 package lv.javaguru.java1.student_anton_pereloma.lesson_3.homework.level_7_senior;
 
-public class Circle {
+class Circle {
+    private double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    double getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+
+    double getRadius() {
+        return this.radius;
+    }
+
 }
--- src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_3/homework/level_7_senior/CircleDemo.java
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_3/homework/level_7_senior/CircleDemo.java
@@ -1,4 +1,9 @@
 package lv.javaguru.java1.student_anton_pereloma.lesson_3.homework.level_7_senior;
 
-public class CircleDemo {
+class CircleDemo {
+    public static void main(String[] args) {
+        Circle firstCircle = new Circle(10);
+        double circleArea = firstCircle.getArea();
+        System.out.println("Circle Area with a radius of " + firstCircle.getRadius() + " is: " + circleArea);
+    }
 }

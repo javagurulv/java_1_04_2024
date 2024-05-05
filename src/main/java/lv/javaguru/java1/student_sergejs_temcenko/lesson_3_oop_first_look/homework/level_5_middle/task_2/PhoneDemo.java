09506c5648f928294f7132f44d903b6bc1087c0d
stemcenko <67960125+stemcenko@users.noreply.github.com>
2024-05-06 00:42:31 +0300

    Lesson_3 homework level_5_middle

---
+++ src/main/java/lv/javaguru/java1/student_sergejs_temcenko/lesson_3_oop_first_look/homework/level_5_middle/task_2/PhoneDemo.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_sergejs_temcenko.lesson_3_oop_first_look.homework.level_5_middle.task_2;
+
+class PhoneDemo {
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+}

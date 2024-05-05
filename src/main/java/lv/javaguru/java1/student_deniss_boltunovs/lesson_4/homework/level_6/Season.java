cfe9724167106758651d0f03943947508338a1cc
Den <deniss.boltunovs@gmail.com>
2024-05-05 19:54:52 +0300

    Lesson 4 homework level_6 task_1

--- src/main/java/lv/javaguru/java1/student_deniss_boltunovs/lesson_4/homework/level_6/Season.java
+++ src/main/java/lv/javaguru/java1/student_deniss_boltunovs/lesson_4/homework/level_6/Season.java
@@ -14,16 +14,16 @@ class Season {
        String season;
 
        switch (month) {
-           case 12: case 1: case 2:
+           case 12, 1, 2:
                season = "Winter";
                break;
-           case 3: case 4: case 5:
+           case 3, 4, 5:
                season = "Spring";
                break;
-           case 6: case 7: case 8:
+           case 6, 7, 8:
                season = "Summer";
                break;
-           case 9: case 10: case 11:
+           case 9, 10, 11:
                season = "Autumn";
                break;
            default:

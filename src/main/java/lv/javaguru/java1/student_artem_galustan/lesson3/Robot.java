30afb275f554a53ce5afa952b801cc698e382248
TheBigG1 <galustjans.artjoms@gmail.com>
2024-05-06 20:23:26 +0300

    Lesson 03 Homework Robot level 1

---
+++ src/main/java/lv/javaguru/java1/student_artem_galustan/lesson3/Robot.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_artem_galustan.lesson3;
+
+public class Robot {
+    private String Name;
+
+    private int work;
+
+    public Robot(String Name, int work){
+        this.Name = Name;
+        this.work = work;
+    }
+}

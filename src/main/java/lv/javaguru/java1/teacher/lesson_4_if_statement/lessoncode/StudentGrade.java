cb910a7100761f1d66d43b5954f1b0163ab105e1
javagurulv <javagurulv@gmail.com>
2024-05-07 21:43:17 +0300

    lesson 4

--- src/main/java/lv/javaguru/java1/teacher/lesson_4_if_statement/lessoncode/StudentGrade.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_4_if_statement/lessoncode/StudentGrade.java
@@ -1,27 +1,21 @@
 package lv.javaguru.java1.teacher.lesson_4_if_statement.lessoncode;
 
-import java.util.Scanner;
-
 class StudentGrade {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int mark = scanner.nextInt();
-
+    public String calculateGrade(int mark) {
         if ((mark >= 90) && (mark <= 100)) {
-            System.out.println("A");
+            return "A";
         } else if ((mark >= 80) && (mark <= 89)) {
-            System.out.println("B");
+            return "B";
         } else if ((mark >= 70) && (mark <= 79)) {
-            System.out.println("C");
+            return "C";
         } else if ((mark >= 60) && (mark <= 69)) {
-            System.out.println("D");
+            return "D";
         } else if ((mark >= 0) && (mark <= 59)) {
-            System.out.println("F");
+            return "F";
+        } else {
+            return "unknown grade";
         }
-
     }
-
 
 }

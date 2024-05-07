f1b5b932262ba609ad761945eab60118eb4c4f8e
jelenaisemja <lenakurasa@gmail.com>
2024-05-07 21:01:27 +0300

    HelloWorld example

---
+++ src/main/java/lv/javaguru/java1/student_jelena_k/lesson1/Number_2.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_jelena_k.lesson1;
+
+public class Number_2 {
+    public static void main(String[] args) {
+        int number1 = 50;
+        int number2 = 3;
+        int result = number1 / number2;
+        System.out.println(result);
+
+
+    }
+}

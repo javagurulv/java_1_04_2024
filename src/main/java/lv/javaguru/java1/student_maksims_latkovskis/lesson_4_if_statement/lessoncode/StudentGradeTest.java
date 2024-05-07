56273398f7ced683e7c34449933454b09d6f9407
TheMaksimys <maksims.latkovskis@gmail.com>
2024-05-07 22:02:05 +0300

    Performed random mark generation

--- src/main/java/lv/javaguru/java1/student_maksims_latkovskis/lesson_4_if_statement/lessoncode/StudentGradeTest.java
+++ src/main/java/lv/javaguru/java1/student_maksims_latkovskis/lesson_4_if_statement/lessoncode/StudentGradeTest.java
@@ -6,6 +6,8 @@ public class StudentGradeTest {
         StudentGradeTest test = new StudentGradeTest();
         test.test1();
         test.test2();
+        for(int i=0; i<10; i++){
+        test.test3();}
     }
 
 
@@ -31,9 +33,20 @@ public class StudentGradeTest {
         {System.out.println("OK");}
     }
 
+    public void test3() {
+        int mark = getRandomNumber(0,59);
+        String expectedGrade = "F";
 
+        StudentGrade studentGrade = new StudentGrade();
+        String realGrade = studentGrade.calculatedMark(mark);
+        System.out.print(mark + "  ");
 
+        if(realGrade == expectedGrade)
+        {System.out.println("OK");}
+    }
 
-
+    public int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
 
 }

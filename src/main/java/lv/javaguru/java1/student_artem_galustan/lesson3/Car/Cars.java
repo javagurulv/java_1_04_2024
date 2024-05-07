d56ca997911bdf1ffadd2c705e64a5619ea77aad
TheBigG1 <galustjans.artjoms@gmail.com>
2024-05-07 19:05:55 +0300

    Lesson 03 Homework Robot level 1

---
+++ src/main/java/lv/javaguru/java1/student_artem_galustan/lesson3/Car/Cars.java
@@ -0,0 +1,26 @@
+package lv.javaguru.java1.student_artem_galustan.lesson3.Car;
+
+public class Cars {
+
+    public String CarName;
+
+    public String Colour;
+
+    public int Price;
+
+    public Cars(String Carname,String Colour, int Price){
+        this.CarName = Carname;
+        this.Colour = Colour;
+        this.Price = Price;
+    }
+
+
+    public String getCarName() {
+
+        return this.CarName;
+    }
+
+    }
+
+
+

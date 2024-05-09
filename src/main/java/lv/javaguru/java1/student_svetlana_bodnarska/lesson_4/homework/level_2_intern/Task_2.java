1ffe452555b160f622a4b33c43014c4a75ff5437
S286B826 <svetlana.bodnarska@inbox.lv>
2024-05-09 21:52:13 +0300

    Lesson_4_level_2

---
+++ src/main/java/lv/javaguru/java1/student_svetlana_bodnarska/lesson_4/homework/level_2_intern/Task_2.java
@@ -0,0 +1,19 @@
+package lv.javaguru.java1.student_svetlana_bodnarska.lesson_4.homework.level_2_intern;
+
+import java.util.Scanner;
+
+class Task_2
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter two even numbers: ");
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+
+        if (number1 < number2){
+            System.out.println(number1);
+        } else if (number2 < number1) {
+            System.out.println(number2);
+        }
+    }
+}

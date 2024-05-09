6801a5291dcb668a3c31958035ab519c967898d5
jelenaisemja <lenakurasa@gmail.com>
2024-05-09 11:10:22 +0300

    HelloWorld example

---
+++ src/main/java/lv/javaguru/java1/student_jelena_k/lesson2/honework2/Task_1/LoginService.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_jelena_k.lesson2.honework2.Task_1;
+
+public class LoginService {
+
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+
+    }
+}

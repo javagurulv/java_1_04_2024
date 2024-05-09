27759b95e7e05ec5a0b343a7f1af1929ea1485a6
jelenaisemja <lenakurasa@gmail.com>
2024-05-09 11:33:19 +0300

    HelloWorld example

---
+++ src/main/java/lv/javaguru/java1/student_jelena_k/lesson4/homework/BooleanVariabl.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_jelena_k.lesson4.homework;
+
+public class BooleanVariabl {
+    public static void main(String[] args) {
+        boolean isTrue = true;
+        boolean isFalse = false;
+        System.out.println(isTrue);
+        System.out.println(isFalse);
+    }
+}

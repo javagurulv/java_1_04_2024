62c7d46f3f3321cdf627d4250eec07b72556c5be
a.eternalstudent <a.eternalstudent@gmail.com>
2024-05-10 22:25:01 +0200

    Completed homework lesson 4 level 6 Task 1

---
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_4/homework/level_6_middle/WhatSeasonApp.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_anton_pereloma.lesson_4.homework.level_6_middle;
+
+import java.util.Scanner;
+
+public class WhatSeasonApp {
+    public static void main(String[] args) {
+        WhatSeason season = new WhatSeason();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter month to get season: ");
+        System.out.print("'Tis " + season.SeasonSelector(scanner.nextInt()));
+    }
+}

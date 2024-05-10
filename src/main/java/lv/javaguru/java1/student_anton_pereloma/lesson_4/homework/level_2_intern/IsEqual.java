11668d749645b9958dc0f9b2ec61a64dbb9f75ad
a.eternalstudent <a.eternalstudent@gmail.com>
2024-05-10 16:17:57 +0200

    Completed homework lesson 4 level 2 Task 3 and level 3 Task 1

---
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_4/homework/level_2_intern/IsEqual.java
@@ -0,0 +1,19 @@
+package lv.javaguru.java1.student_anton_pereloma.lesson_4.homework.level_2_intern;
+
+import java.util.Scanner;
+
+public class IsEqual {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int number1 = scanner.nextInt();
+        System.out.println("Enter second number: ");
+        int number2 = scanner.nextInt();
+
+        if (number1 != number2) {
+            System.out.println("Numbers are NOT equal");
+        } else {
+            System.out.println("Numbers are equal");
+        }
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_4/homework/level_3_junior/GreaterOfThree.java
@@ -0,0 +1,24 @@
+package lv.javaguru.java1.student_anton_pereloma.lesson_4.homework.level_3_junior;
+
+import java.util.Scanner;
+
+public class GreaterOfThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first variable X:");
+        int x = scanner.nextInt();
+        System.out.println("Enter the second variable Y:");
+        int y = scanner.nextInt();
+        System.out.println("Enter the third variable Z:");
+        int z = scanner.nextInt();
+        if ((x > y) && (x > z)) {
+            System.out.println("X is the greatest number");
+        } else if ((y > x) && (y > z)) {
+            System.out.println("Y is the greatest number");
+        } else if ((z > x) && (z > y)) {
+            System.out.println("Z is the greatest number");
+        } else {
+            System.out.println("Numbers are equal");
+        }
+    }
+}

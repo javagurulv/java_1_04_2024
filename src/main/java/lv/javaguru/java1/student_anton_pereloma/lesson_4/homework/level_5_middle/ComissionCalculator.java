766c32f3cc0802a3f947f26363b03778e8b992b2
a.eternalstudent <a.eternalstudent@gmail.com>
2024-05-10 22:08:12 +0200

    Completed homework lesson 4 level 5 Task 1

---
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_4/homework/level_5_middle/ComissionCalculator.java
@@ -0,0 +1,22 @@
+package lv.javaguru.java1.student_anton_pereloma.lesson_4.homework.level_5_middle;
+
+class CommissionCalculator {
+    int withdrawal;
+
+    CommissionCalculator (int withdrawal) {
+        this.withdrawal = withdrawal;
+    }
+
+    public double getPercentage() {
+        int percentage = 0;
+        if (withdrawal < 1000) {
+            percentage = 3;
+        } else if ((withdrawal >= 1000) && (withdrawal < 5000)) {
+            percentage = 4;
+        } else if (withdrawal >= 5000) {
+            percentage = 5;
+        }
+        return (double) percentage/100 ;
+    }
+
+}

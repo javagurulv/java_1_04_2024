9f5100312e1c2abd990c08c9203ac4caecfed2b5
NSnadezhda <nadezda.senko@gmail.com>
2024-05-11 16:13:02 +0300

    lesson4 level2

---
+++ src/main/java/lv/javaguru/java1/student_nadezda_radigina/lesson_4/level_2_intern/Task3.java
@@ -0,0 +1,21 @@
+package lv.javaguru.java1.student_nadezda_radigina.lesson_4.level_2_intern;
+
+import java.util.Scanner;
+
+class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number1: ");
+        int number1 = scanner.nextInt();
+
+        System.out.println("Enter number2: ");
+        int number2 = scanner.nextInt();
+
+        if (number1 != number2){
+            System.out.println("Numbers are different");
+        } else {
+            System.out.println("Numbers are equals");
+        }
+    }
+}

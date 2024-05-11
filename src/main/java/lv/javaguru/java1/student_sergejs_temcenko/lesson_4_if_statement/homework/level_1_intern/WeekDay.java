2094f794e5839a0c0420259381e5abac26590984
stemcenko <67960125+stemcenko@users.noreply.github.com>
2024-05-11 23:06:08 +0300

    Lesson_4 homework level_1_intern task_1

---
+++ src/main/java/lv/javaguru/java1/student_sergejs_temcenko/lesson_4_if_statement/homework/level_1_intern/WeekDay.java
@@ -0,0 +1,31 @@
+package lv.javaguru.java1.student_sergejs_temcenko.lesson_4_if_statement.homework.level_1_intern;
+
+import lv.javaguru.java1.teacher.lesson_6_unit_testing.homework.level_6_middle.SalaryTaxCalculatorApp;
+
+import java.util.Scanner;
+
+class WeekDay {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        if (number == 1) {
+            System.out.println("Monday");
+        } else if (number == 2) {
+            System.out.println("Tuesday");
+        } else if (number == 3) {
+            System.out.println("Wednesday");
+        } else if (number == 4) {
+            System.out.println("Thursday");
+        } else if (number == 5) {
+            System.out.println("Friday");
+        } else if (number == 6) {
+            System.out.println("Saturday");
+        } else if (number == 7) {
+            System.out.println("Sunday");
+        } else {
+            System.out.println("There is no such day...");
+        }
+    }
+}

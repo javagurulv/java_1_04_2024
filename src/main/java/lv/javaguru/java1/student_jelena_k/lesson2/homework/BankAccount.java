fa82184440ceed4da85495bb83f89995f5975955
jelenaisemja <lenakurasa@gmail.com>
2024-05-11 13:28:30 +0300

    HelloWorld example

--- src/main/java/lv/javaguru/java1/student_jelena_k/lesson2/homework/BankAccount.java
+++ src/main/java/lv/javaguru/java1/student_jelena_k/lesson2/homework/BankAccount.java
@@ -1,8 +1,15 @@
 package lv.javaguru.java1.student_jelena_k.lesson2.homework;
 
 public class BankAccount {
+    private String ownerName;
+    private int moneyAmount;
 
-    public static void main(String[] args) {
-
+    public BankAccount(String ownerName, int moneyAmount) {
+        this.ownerName = ownerName;
+        this.moneyAmount = moneyAmount;
     }
+
+    public String getOwnerName() { return this.ownerName: }
+
+    public int getMoneyAmount() { return moneyAmount: }
 }

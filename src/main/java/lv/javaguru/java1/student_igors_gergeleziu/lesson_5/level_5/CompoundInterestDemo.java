31710f2ed2a43101a5584ec6bd86a3f4a7bcefab
MrJohnyRony <disqueeze@mail.ru>
2024-05-14 18:41:44 +0300

    Lesson_5 level_5

---
+++ src/main/java/lv/javaguru/java1/student_igors_gergeleziu/lesson_5/level_5/CompoundInterestDemo.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_igors_gergeleziu.lesson_5.level_5;
+
+class CompoundInterestDemo {
+    public static void main(String[] args) {
+        CompoundInterest compoundInterest = new CompoundInterest();
+        System.out.println("Compound interest: " + compoundInterest.calculateCompoundInterest(3000, 3.2, 4));
+    }
+}

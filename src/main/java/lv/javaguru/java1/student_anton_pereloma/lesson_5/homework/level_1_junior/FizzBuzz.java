758bf1e5460afdf2d454aa15b7eb0dc9abf31f12
a.eternalstudent <a.eternalstudent@gmail.com>
2024-05-15 06:47:39 +0200

    Copied follow along material from Lesson 5, completed lesson 5 HW level 1 task 1

---
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_5/homework/level_1_junior/FizzBuzz.java
@@ -0,0 +1,25 @@
+package lv.javaguru.java1.student_anton_pereloma.lesson_5.homework.level_1_junior;
+
+class FizzBuzz {
+    String toFizzBuzz(int number) {
+        String result;
+        if ((divisibleBy3(number) && (divisibleBy5(number)))) {
+            result = "FizzBuzz";
+        } else if (divisibleBy3(number)) {
+            result = "Fizz";
+        } else if (divisibleBy5(number)) {
+            result = "Fizz";
+        } else {
+            result = String.valueOf(number);
+        }
+        return result;
+    }
+
+    boolean divisibleBy3(int number) {
+        return (number % 3) == 0;
+    }
+
+    boolean divisibleBy5(int number) {
+        return (number % 5) == 0;
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_5/homework/level_1_junior/FizzBuzzApp.java
@@ -0,0 +1,16 @@
+package lv.javaguru.java1.student_anton_pereloma.lesson_5.homework.level_1_junior;
+
+class FizzBuzzApp {
+    public static void main(String[] args) {
+
+        FizzBuzz firstNumber = new FizzBuzz();
+
+        System.out.println(firstNumber.toFizzBuzz(15));
+        System.out.println(firstNumber.toFizzBuzz(17));
+        System.out.println(firstNumber.toFizzBuzz(11));
+        System.out.println(firstNumber.toFizzBuzz(5));
+        System.out.println(firstNumber.toFizzBuzz(1));
+        System.out.println(firstNumber.toFizzBuzz(3));
+        System.out.println(firstNumber.toFizzBuzz(0));
+    }
+}

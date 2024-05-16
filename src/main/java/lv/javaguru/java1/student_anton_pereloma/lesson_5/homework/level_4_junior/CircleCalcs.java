86a1e7aa73fdcb224550b746cc1792415b061b6f
a.eternalstudent <a.eternalstudent@gmail.com>
2024-05-16 21:18:26 +0200

    Refactored Lesson 5 level 4 task package location

--- src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_5/homework/level_4_junior/CircleCalcs.java
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_5/homework/level_4_junior/CircleCalcs.java
@@ -1,4 +1,4 @@
-package lv.javaguru.java1.student_anton_pereloma.lesson_5.homework.CircleCalculations;
+package lv.javaguru.java1.student_anton_pereloma.lesson_5.homework.level_4_junior;
 
 class CircleCalcs {
     double findPerimeter(double radius) {

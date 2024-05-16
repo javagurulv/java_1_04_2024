25bee7738422543e3e51ddedf854498d0ed30a90
a.eternalstudent <a.eternalstudent@gmail.com>
2024-05-16 21:16:49 +0200

    Completed Lesson 5 level 4 task

---
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_5/homework/CircleCalculations/CircleCalcs.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_anton_pereloma.lesson_5.homework.CircleCalculations;
+
+class CircleCalcs {
+    double findPerimeter(double radius) {
+        return 2 * radius * Math.PI;
+    }
+    double findArea(double radius) {
+        return radius * radius * Math.PI;
+    }
+
+
+}

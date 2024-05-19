f1032cbdfb0e2cd0c328bab6c99981cb5d8ff6b5
Natalia Kochkina <tisha.silence12@gmail.com>
2024-05-19 22:38:31 +0300

    lesson 7 homework level 1

---
+++ src/main/java/lv/javaguru/java1/student_natalia_kochkina/lesson_7/homework/level_1/ArrayDefinition2.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_natalia_kochkina.lesson_7.homework.level_1;
+
+class ArrayDefinition2 {
+
+    public static void main(String[] args) {
+        byte[] numbers = new byte[10];
+    }
+
+}

aa10bb1db991eb80e78f189e080b7904aa49c439
TheMaksimys <maksims.latkovskis@gmail.com>
2024-05-19 23:57:41 +0300

    Created first program

---
+++ src/main/java/lv/javaguru/java1/student_maksims_latkovskis/lesson_4_if_statement/homework/level_1_intern/ParityCheck.java
@@ -0,0 +1,26 @@
+package lv.javaguru.java1.student_maksims_latkovskis.lesson_4_if_statement.homework.level_1_intern;
+
+import java.util.Scanner;
+
+
+    import java.util.Scanner;
+
+    class ParityCheck {
+
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Please enter the integer:");
+            int inputNumber = scanner.nextInt();
+            boolean isEven = (inputNumber%2 ==0);
+
+            if (isEven){
+                System.out.println("Number is even");
+            }
+
+            else{
+                System.out.println("Number is odd");
+            }
+        }
+    }

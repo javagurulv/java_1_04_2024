913cc6fbf63b7f9e01514d5d1910773c4ab88593
stemcenko <67960125+stemcenko@users.noreply.github.com>
2024-05-19 21:51:49 +0300

    Lesson_5 homework level_1_intern task_2

---
+++ src/main/java/lv/javaguru/java1/student_sergejs_temcenko/lesson_5_methods/homework/level_1_intern/ColorSpectrumAplication.java
@@ -0,0 +1,14 @@
+package lv.javaguru.java1.student_sergejs_temcenko.lesson_5_methods.homework.level_1_intern;
+
+class ColorSpectrumAplication {
+    public static void main(String[] args) {
+        ColorSpectrum colorSpectrum = new ColorSpectrum();
+
+        System.out.println(colorSpectrum.colorSpectrumWaveLength(440));
+        System.out.println(colorSpectrum.colorSpectrumWaveLength(498));
+        System.out.println(colorSpectrum.colorSpectrumWaveLength(460));
+        System.out.println(colorSpectrum.colorSpectrumWaveLength(605));
+        System.out.println(colorSpectrum.colorSpectrumWaveLength(628));
+        System.out.println(colorSpectrum.colorSpectrumWaveLength(100));
+    }
+}

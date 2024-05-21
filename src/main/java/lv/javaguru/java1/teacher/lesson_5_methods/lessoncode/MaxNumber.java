a0cefac8458edc970d67d0ac75293568f7b995ac
javagurulv <javagurulv@gmail.com>
2024-05-21 21:37:01 +0300

    lesson 5 Calculator

--- src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/MaxNumber.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/MaxNumber.java
@@ -2,6 +2,12 @@ package lv.javaguru.java1.teacher.lesson_5_methods.lessoncode;
 
 class MaxNumber {
 
+    int findMax(int number1, int number2, int number3) {
+        int maxNumber = findMax(number1, number2);
+        maxNumber = findMax(maxNumber, number3);
+        return maxNumber;
+    }
+
     int findMax(int number1, int number2) {
         return Math.max(number1, number2);
 
--- src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/MaxNumberTest.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/MaxNumberTest.java
@@ -7,6 +7,13 @@ class MaxNumberTest {
         test.test1();
         test.test2();
         test.test3();
+        test.test4();
+        test.test5();
+        test.test6();
+        test.test7();
+        test.test8();
+        test.test9();
+        test.test10();
     }
 
     // 2, 5 -> 5
@@ -41,5 +48,83 @@ class MaxNumberTest {
             System.out.println("TEST 3 = FAIL");
         }
     }
+
+    //1,2,3 -> 3
+    public void test4() {
+        MaxNumber maxNumber = new MaxNumber();
+        int result = maxNumber.findMax(1,2,3);
+        if (result == 3) {
+            System.out.println("TEST 4 = OK");
+        } else {
+            System.out.println("TEST 4 = FAIL");
+        }
+    }
+
+    //1,3,2 -> 3
+    public void test5() {
+        MaxNumber maxNumber = new MaxNumber();
+        int result = maxNumber.findMax(1,3,2);
+        if (result == 3) {
+            System.out.println("TEST 5 = OK");
+        } else {
+            System.out.println("TEST 5 = FAIL");
+        }
+    }
+
+    //3,2,1 -> 3
+    public void test6() {
+        MaxNumber maxNumber = new MaxNumber();
+        int result = maxNumber.findMax(3,2,1);
+        if (result == 3) {
+            System.out.println("TEST 6 = OK");
+        } else {
+            System.out.println("TEST 6 = FAIL");
+        }
+    }
+
+    //3,3,1 -> 3
+    public void test7() {
+        MaxNumber maxNumber = new MaxNumber();
+        int result = maxNumber.findMax(3,3,1);
+        if (result == 3) {
+            System.out.println("TEST 7 = OK");
+        } else {
+            System.out.println("TEST 7 = FAIL");
+        }
+    }
+
+    //3,1,3 -> 3
+    public void test8() {
+        MaxNumber maxNumber = new MaxNumber();
+        int result = maxNumber.findMax(3,1,3);
+        if (result == 3) {
+            System.out.println("TEST 8 = OK");
+        } else {
+            System.out.println("TEST 8 = FAIL");
+        }
+    }
+
+    //1,3,3 -> 3
+    public void test9() {
+        MaxNumber maxNumber = new MaxNumber();
+        int result = maxNumber.findMax(1,3,3);
+        if (result == 3) {
+            System.out.println("TEST 9 = OK");
+        } else {
+            System.out.println("TEST 9 = FAIL");
+        }
+    }
+
+    //3,3,3 -> 3
+    public void test10() {
+        MaxNumber maxNumber = new MaxNumber();
+        int result = maxNumber.findMax(3,3,3);
+        if (result == 3) {
+            System.out.println("TEST 10 = OK");
+        } else {
+            System.out.println("TEST 10 = FAIL");
+        }
+    }
+
 
 }

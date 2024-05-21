0012f0c0d12acd9d0c30b9c9ed07fc0e330775ab
javagurulv <javagurulv@gmail.com>
2024-05-21 20:33:13 +0300

    lesson 5 Calculator

--- src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/Calculator.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/Calculator.java
@@ -10,4 +10,16 @@ class Calculator {
         return (number % 2) != 0;
     }
 
+    int findAverage(int number1, int number2) {
+        int sum = number1 + number2;
+        return sum / 2;
+
+        //return (number1 + number2) / 2;
+    }
+
+    double findAverageDouble(int number1, int number2) {
+        int sum = number1 + number2;
+        return (double) sum / 2;
+    }
+
 }
--- src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/CalculatorApp.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_5_methods/lessoncode/CalculatorApp.java
@@ -12,8 +12,13 @@ class CalculatorApp {
         boolean evenResult = calculator.isEven(number);
         boolean oddResult = calculator.isOdd(number);
 
+        int averageResult = calculator.findAverage(4, 5);
+        double averageResultDouble = calculator.findAverageDouble(4, 5);
+
         System.out.println("Is even = " + evenResult);
         System.out.println("Is odd = " + oddResult);
+        System.out.println("Is average = " + averageResult);
+        System.out.println("Is average double = " + averageResultDouble);
     }
 
 }

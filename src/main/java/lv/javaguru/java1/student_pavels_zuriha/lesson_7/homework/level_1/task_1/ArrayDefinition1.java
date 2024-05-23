c578a099209bad53364e2b56d955c51416f00839
Pahanec <87240194+Pahanec@users.noreply.github.com>
2024-05-23 18:44:13 +0300

    Lesson 7 homework level_1

---
+++ src/main/java/lv/javaguru/java1/student_pavels_zuriha/lesson_7/homework/level_1/task_1/ArrayDefinition1.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_pavels_zuriha.lesson_7.homework.level_1.task_1;
+
+class ArrayDefinition1 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+    }
+
+}
---
+++ src/main/java/lv/javaguru/java1/student_pavels_zuriha/lesson_7/homework/level_1/task_8/ArrayLength3.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_pavels_zuriha.lesson_7.homework.level_1.task_8;
+
+class ArrayLength3 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers.length);
+    }
+
+}

22e3fad9876fdb6115ea4ff908f47497779434aa
Natalia Kochkina <tisha.silence12@gmail.com>
2024-05-23 21:37:56 +0300

    lesson 5 homework

--- src/main/java/lv/javaguru/java1/student_natalia_kochkina/lesson_5/homework/level_4/CirclePerimeterAndArea.java
+++ src/main/java/lv/javaguru/java1/student_natalia_kochkina/lesson_5/homework/level_4/CirclePerimeterAndArea.java
@@ -1,12 +1,19 @@
 package lv.javaguru.java1.student_natalia_kochkina.lesson_5.homework.level_4;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 class CirclePerimeterAndArea {
 
-    public double perimeter(double radius) {
-        return 2 * Math.PI * radius;
+    BigDecimal calculatePerimeter(BigDecimal radius) {
+        BigDecimal pi = new BigDecimal(Math.PI);
+        BigDecimal perimeter = new BigDecimal("2").multiply(radius).multiply(pi);
+        return perimeter.setScale(2, RoundingMode.HALF_UP);
     }
 
-    public double area(double radius) {
-        return Math.PI * radius * radius;
+    BigDecimal calculateArea(BigDecimal radius) {
+        BigDecimal pi = new BigDecimal(Math.PI);
+        BigDecimal area = pi.multiply(radius).multiply(radius);
+        return area.setScale(2, RoundingMode.HALF_UP);
     }
 }

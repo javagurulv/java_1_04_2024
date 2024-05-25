6171ae064a21138ee7b880e8c0fa15ab94270de8
Artjoms <galustjans.artjoms@gmail.com>
2024-05-25 22:32:55 +0300

    Lesson 4

--- src/main/java/lv/javaguru/java1/student_artem_galustan/lesson4/homework/EvenOrOddRequest.java
+++ src/main/java/lv/javaguru/java1/student_artem_galustan/lesson4/homework/EvenOrOddRequest.java
@@ -6,15 +6,16 @@ public class EvenOrOddRequest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите целое число: ");
+        System.out.print("напиши 1 число: ");
 
         int Number = scanner.nextInt();
+        int NumberTwo = scanner.nextInt();
 
 
         if(Number %2 == 0){
-            System.out.println("Число " + Number + " является четным.");
+            System.out.println("Число " + NumberTwo + " является четным.");
         } else {
-            System.out.println("Число " + Number + " является нечетным.");
+            System.out.println("Число " + NumberTwo + " является нечетным.");
         }
 
         scanner.close();

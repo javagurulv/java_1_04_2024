d838cef90e12a7281953d7516287bde8655b0305
Den <deniss.boltunovs@gmail.com>
2024-05-25 19:54:37 +0300

    Lesson 6 homework level_4

--- src/main/java/lv/javaguru/java1/student_deniss_boltunovs/lesson_6/homework/level_4/FactorialForLoopTest.java
+++ src/main/java/lv/javaguru/java1/student_deniss_boltunovs/lesson_6/homework/level_4/FactorialForLoopTest.java
@@ -4,48 +4,44 @@ class FactorialForLoopTest {
 
     public static void main(String[] args) {
         FactorialForLoopTest test = new FactorialForLoopTest();
-        test.testFactorialOf5();
-        test.testFactorialOf3();
-        test.testFactorialOf0();
+        test.test1();
+        test.test2();
+        test.test3();
     }
 
-    // input  : 5
-    // output : 120
-    public void testFactorialOf5(){
+    //  number = 5, result = 120
+    public void test1(){
         FactorialForLoop factorial = new FactorialForLoop();
         int result = factorial.factorial(5);
         int expectedResult = 120;
-        if (result == expectedResult) {
-            System.out.println("TEST FactorialOf5: OK");
-        } else {
-            System.out.println("TEST FactorialOf5: FAILED (expected " + expectedResult + ", received " + result +")");
-        }
+        checkResult(result, expectedResult, "test1");
     }
 
-    // input  : 3
-    // output : 6
-    public void testFactorialOf3(){
+    //  number = 3, result = 6
+    public void test2(){
         FactorialForLoop factorial = new FactorialForLoop();
         int result = factorial.factorial(3);
         int expectedResult = 6;
-        if (result == expectedResult) {
-            System.out.println("TEST FactorialOf3: OK");
-        } else {
-            System.out.println("TEST FactorialOf3: FAILED (expected " + expectedResult + ", received " + result +")");
-        }
+        checkResult(result, expectedResult, "test2");
     }
 
-    // input  : 0
-    // output : 1
-    public void testFactorialOf0(){
+    //  number = 0, result = 1
+    public void test3(){
         FactorialForLoop factorial = new FactorialForLoop();
         int result = factorial.factorial(0);
         int expectedResult = 1;
-        if (result == expectedResult) {
-            System.out.println("TEST FactorialOf0: OK");
+        checkResult(result, expectedResult, "test3");
+    }
+
+    public void checkResult(int realResult,
+                            int expectedResult,
+                            String testName) {
+        if (realResult == expectedResult) {
+            System.out.println(testName + " : OK");
         } else {
-            System.out.println("TEST FactorialOf0: FAILED (expected " + expectedResult + ", received " + result +")");
+            System.out.println(testName + " : FAILED" + " (result " + realResult + ", expected " + expectedResult + ")");
         }
     }
+
 
 }
--- src/main/java/lv/javaguru/java1/student_deniss_boltunovs/lesson_6/homework/level_4/FactorialRecursionTest.java
+++ src/main/java/lv/javaguru/java1/student_deniss_boltunovs/lesson_6/homework/level_4/FactorialRecursionTest.java
@@ -3,48 +3,43 @@ package lv.javaguru.java1.student_deniss_boltunovs.lesson_6.homework.level_4;
 class FactorialRecursionTest {
 
     public static void main(String[] args) {
-      FactorialRecursionTest test = new FactorialRecursionTest();
-      test.testFactorialOf5();
-      test.testFactorialOf3();
-      test.testFactorialOf0();
+        FactorialRecursionTest test = new FactorialRecursionTest();
+        test.test1();
+        test.test2();
+        test.test3();
     }
 
-    // input  : 5
-    // output : 120
-    public void testFactorialOf5(){
+    //  number = 5, result = 120
+    public void test1(){
         FactorialRecursion factorial = new FactorialRecursion();
         int result = factorial.factorial(5);
         int expectedResult = 120;
-        if (result == expectedResult) {
-            System.out.println("TEST FactorialOf5: OK");
-        } else {
-            System.out.println("TEST FactorialOf5: FAILED (expected " + expectedResult + ", received " + result +")");
-        }
+        checkResult(result, expectedResult, "test1");
     }
 
-    // input  : 3
-    // output : 6
-    public void testFactorialOf3(){
+    //  number = 3, result = 6
+    public void test2(){
         FactorialRecursion factorial = new FactorialRecursion();
         int result = factorial.factorial(3);
         int expectedResult = 6;
-        if (result == expectedResult) {
-            System.out.println("TEST FactorialOf3: OK");
-        } else {
-            System.out.println("TEST FactorialOf3: FAILED (expected " + expectedResult + ", received " + result +")");
-        }
+        checkResult(result, expectedResult, "test2");
     }
 
-    // input  : 0
-    // output : 1
-    public void testFactorialOf0(){
+    //  number = 0, result = 1
+    public void test3(){
         FactorialRecursion factorial = new FactorialRecursion();
         int result = factorial.factorial(0);
         int expectedResult = 1;
-        if (result == expectedResult) {
-            System.out.println("TEST FactorialOf0: OK");
+        checkResult(result, expectedResult, "test3");
+    }
+
+    public void checkResult(int realResult,
+                            int expectedResult,
+                            String testName) {
+        if (realResult == expectedResult) {
+            System.out.println(testName + " : OK");
         } else {
-            System.out.println("TEST FactorialOf0: FAILED (expected " + expectedResult + ", received " + result +")");
+            System.out.println(testName + " : FAILED" + " (result " + realResult + ", expected " + expectedResult + ")");
         }
     }
 

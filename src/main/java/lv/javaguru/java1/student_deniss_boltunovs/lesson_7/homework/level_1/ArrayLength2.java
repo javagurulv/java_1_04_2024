4bcf7acefa1bbdb239c65c11e0d1194e81e7b099
Den <deniss.boltunovs@gmail.com>
2024-05-27 20:20:19 +0300

    Lesson 7 homework level_1

---
+++ src/main/java/lv/javaguru/java1/student_deniss_boltunovs/lesson_7/homework/level_1/ArrayLength2.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_deniss_boltunovs.lesson_7.homework.level_1;
+
+class ArrayLength2 {
+
+        public static void main(String[] args) {
+            int[] numbers = new int[3];
+            System.out.println(numbers[0]);
+            System.out.println(numbers[1]);
+            System.out.println(numbers[2]);
+        }
+
+}
+

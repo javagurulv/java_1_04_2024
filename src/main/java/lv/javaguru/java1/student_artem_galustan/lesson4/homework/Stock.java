9ae10dcc3b7262f64a5c66777c404c7a36337cc0
Artjoms <galustjans.artjoms@gmail.com>
2024-05-27 10:22:59 +0300

    Lesson 4

--- src/main/java/lv/javaguru/java1/student_artem_galustan/lesson4/homework/Stock.java
+++ src/main/java/lv/javaguru/java1/student_artem_galustan/lesson4/homework/Stock.java
@@ -2,7 +2,7 @@ package lv.javaguru.java1.student_artem_galustan.lesson4.homework;
 
 public class Stock {
     public static void main(String[] args) {
-        Stock NewStock = new Stock("BItok" 3)
+
     }
 
         String Name;

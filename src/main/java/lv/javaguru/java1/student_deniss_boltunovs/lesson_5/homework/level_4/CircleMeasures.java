89b801006eff352a208da1bb2ef8006eb10ecf42
Den <deniss.boltunovs@gmail.com>
2024-05-27 08:19:59 +0300

    Lesson 5 homework adjustment

--- src/main/java/lv/javaguru/java1/student_deniss_boltunovs/lesson_5/homework/level_4/CircleMeasures.java
+++ src/main/java/lv/javaguru/java1/student_deniss_boltunovs/lesson_5/homework/level_4/CircleMeasures.java
@@ -2,15 +2,13 @@ package lv.javaguru.java1.student_deniss_boltunovs.lesson_5.homework.level_4;
 
 class CircleMeasures {
 
-    double PI = Math.PI;
-
     public void circlePerimeter(double radius) {
-        double perimeter = 2 * PI * radius;
+        double perimeter = 2 * Math.PI * radius;
         System.out.println("Circle perimeter: " + perimeter);
     }
 
     public void circleArea(double radius) {
-        double area = PI * Math.pow(radius, 2);
+        double area = Math.PI * Math.pow(radius, 2);
         System.out.println("Circle area: " + area);
     }
 

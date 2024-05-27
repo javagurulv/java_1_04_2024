40f5f6b4fff1fe29d865ab45d0062f16df47e69b
Natalia Kochkina <tisha.silence12@gmail.com>
2024-05-27 23:39:38 +0300

    project1_fraud_detector steps 1,2

---
+++ src/main/java/lv/javaguru/java1/student_natalia_kochkina/project1_fraud_detector/FraudDetector.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_natalia_kochkina.project1_fraud_detector;
+
+class FraudDetector {
+
+    boolean isFraud(Transaction transaction) {
+        return true;
+    }
+
+}

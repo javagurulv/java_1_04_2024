10c95d16edbb854bf939fc7bf94b0382cd3fbca3
Natalia Kochkina <tisha.silence12@gmail.com>
2024-05-29 00:19:52 +0300

    FraudDetector steps 11, 12

--- src/main/java/lv/javaguru/java1/student_natalia_kochkina/project1_fraud_detector/FraudDetector.java
+++ src/main/java/lv/javaguru/java1/student_natalia_kochkina/project1_fraud_detector/FraudDetector.java
@@ -8,12 +8,23 @@ class FraudDetector {
     private FraudRule rule4 = new FraudRule4();
     private FraudRule rule5 = new FraudRule5();
 
-    boolean isFraud(Transaction transaction) {
-        return rule1.isFraud(transaction)
-                || rule2.isFraud(transaction)
-                || rule3.isFraud(transaction)
-                || rule4.isFraud(transaction)
-                || rule5.isFraud(transaction);
+    FraudDetectionResult isFraud(Transaction transaction) {
+        if (rule1.isFraud(transaction)) {
+            return new FraudDetectionResult(true, "FraudRule1");
+        }
+        if (rule2.isFraud(transaction)) {
+            return new FraudDetectionResult(true, "FraudRule2");
+        }
+        if (rule3.isFraud(transaction)) {
+            return new FraudDetectionResult(true, "FraudRule3");
+        }
+        if (rule4.isFraud(transaction)) {
+            return new FraudDetectionResult(true, "FraudRule4");
+        }
+        if (rule5.isFraud(transaction)) {
+            return new FraudDetectionResult(true, "FraudRule5");
+        }
+        return new FraudDetectionResult(false, null);
     }
 
 }

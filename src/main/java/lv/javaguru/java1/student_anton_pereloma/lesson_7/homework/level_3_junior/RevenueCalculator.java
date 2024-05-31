69d30b716f12d8e6c19f588848555a577bba6cd6
a.eternalstudent <a.eternalstudent@gmail.com>
2024-05-31 18:35:20 +0200

    Added working Lesson 7 Level 3 Task 1

--- src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_7/homework/level_3_junior/RevenueCalculator.java
+++ src/main/java/lv/javaguru/java1/student_anton_pereloma/lesson_7/homework/level_3_junior/RevenueCalculator.java
@@ -1,13 +1,17 @@
-//package lv.javaguru.java1.student_anton_pereloma.lesson_7.homework.level_3_junior;
-//
-//class RevenueCalculator {
-//
-//    double calculateTotalRevenue() {
-//        double totalRevenue = 0;
-//        return  totalRevenue;
-//    }
-//
-//    void calculateMeanRevenue(double[] array) {
-//        System.out.println(array[0]);
-//    }
-//}
+package lv.javaguru.java1.student_anton_pereloma.lesson_7.homework.level_3_junior;
+
+class RevenueCalculator {
+
+    double calculateTotalRevenue(PaymentReceipt[] array) {
+        double totalRevenue = 0;
+        for (int i = 0; i < array.length; i++){
+            totalRevenue = totalRevenue + array[i].getPurchaseAmount();
+        }
+        return  totalRevenue;
+    }
+
+    double calculateMeanRevenue(PaymentReceipt[] array) {
+        double mean = calculateTotalRevenue(array);
+        return mean/array.length;
+    }
+}

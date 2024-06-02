ac4b521078e046bd3b30f6fde2ab4a46f18176e7
Den <deniss.boltunovs@gmail.com>
2024-06-02 10:19:20 +0300

    project_1_fraud_detector step_2

--- src/main/java/lv/javaguru/java1/student_deniss_boltunovs/project_1_fraud_detector/Trader.java
+++ src/main/java/lv/javaguru/java1/student_deniss_boltunovs/project_1_fraud_detector/Trader.java
@@ -5,4 +5,14 @@ class Trader {
     private String fullName;
     private String city;
 
+    Trader(String fullName, String city) {
+        this.fullName = fullName;
+        this.city =  city;
+    }
+
+    public String getFullName() {return fullName;}
+
+    public String getCity() {return city;}
+
+
 }
--- src/main/java/lv/javaguru/java1/student_deniss_boltunovs/project_1_fraud_detector/Transaction.java
+++ src/main/java/lv/javaguru/java1/student_deniss_boltunovs/project_1_fraud_detector/Transaction.java
@@ -5,4 +5,14 @@ class Transaction {
     private Trader trader;
     private int amount;
 
+    Transaction(Trader trader, int amount) {
+        this.trader = trader;
+        this.amount = amount;
+    }
+
+    public Trader getTrader(){return  trader;}
+
+    public int getAmount() {return amount;}
+
+
 }

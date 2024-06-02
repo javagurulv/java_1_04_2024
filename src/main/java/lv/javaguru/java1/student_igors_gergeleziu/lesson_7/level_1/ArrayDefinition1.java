7cab1fa6aa9d163f3612034df76c1f686db05d0b
MrJohnyRony <disqueeze@mail.ru>
2024-06-02 17:25:12 +0300

    Lesson_7 level_1 homework

---
+++ src/main/java/lv/javaguru/java1/student_igors_gergeleziu/lesson_7/level_1/ArrayDefinition1.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_igors_gergeleziu.lesson_7.level_1;
+
+class ArrayDefinition1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+    }
+
+}

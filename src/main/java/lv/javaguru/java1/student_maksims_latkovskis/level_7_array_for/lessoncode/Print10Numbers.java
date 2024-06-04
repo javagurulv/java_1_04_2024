37f7244378cf1213ee096795dff3e3c55a0433cd
TheMaksimys <maksims.latkovskis@gmail.com>
2024-06-04 21:03:09 +0300

    Created new solution

---
+++ src/main/java/lv/javaguru/java1/student_maksims_latkovskis/level_7_array_for/lessoncode/Print10Numbers.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_maksims_latkovskis.level_7_array_for.lessoncode;
+
+class Print10Numbers {
+
+    void print(){
+
+        for(int i =1; i<11; i++){
+            System.out.print(i + " ");
+        }
+
+    }
+}

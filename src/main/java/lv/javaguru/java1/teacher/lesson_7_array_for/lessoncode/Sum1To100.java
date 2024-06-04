e63d470a696b562ce8a6840d26250e279de09beb
javagurulv <javagurulv@gmail.com>
2024-06-04 21:48:06 +0300

    lesson 7

--- src/main/java/lv/javaguru/java1/teacher/lesson_7_array_for/lessoncode/Sum1To100.java
+++ src/main/java/lv/javaguru/java1/teacher/lesson_7_array_for/lessoncode/Sum1To100.java
@@ -6,7 +6,11 @@ class Sum1To100 {
         int sum = 0;
         for (int i = 1; i <= 100; i++) {
             sum = sum + i;
-            System.out.println(sum);
+
+            System.out.println("i = " + i);
+            System.out.println("Sum = " + sum);
+
+            System.out.println("----------");
         }
         return sum;
     }

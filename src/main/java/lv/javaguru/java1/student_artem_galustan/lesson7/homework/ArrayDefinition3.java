76618e2220f7e030665cab04aed2661970e2727c
Artjoms <galustjans.artjoms@gmail.com>
2024-06-05 20:44:12 +0300

    Lesson 7

---
+++ src/main/java/lv/javaguru/java1/student_artem_galustan/lesson7/homework/ArrayDefinition3.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_artem_galustan.lesson7.homework;
+
+class ArrayDefinition3 {
+
+        public static void main(String[] args) {
+            int[] numbers = {1, 2, 3};
+        }
+
+    }
+
+// Тут есть решения 2 способами это непосредственно при инициализации массива с элементами,нужно использовать оператор new и указать тип массива.
+// int[] numbers = new int[]{1, 2, 3};
+// либо выше указанный вариант.

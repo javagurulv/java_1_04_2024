9e053c749a8e8b8a2a731297b75ff2209074cd2c
amezateru <amezateru@gmail.com>
2024-06-05 21:24:17 +0300

    Lesson3

---
+++ src/main/java/lv/javaguru/java1/student_nikita_paramonovs/lesson_7/Task_7.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_nikita_paramonovs.lesson_7;
+
+public class Task_7 {
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+    }
+}

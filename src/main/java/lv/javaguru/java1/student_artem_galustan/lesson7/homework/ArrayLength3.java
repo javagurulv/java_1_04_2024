2db2d53da3ecfe51e2f6cc698490d16970b048e9
Artjoms <galustjans.artjoms@gmail.com>
2024-06-05 21:24:15 +0300

    Lesson 7

---
+++ src/main/java/lv/javaguru/java1/student_artem_galustan/lesson7/homework/ArrayLength3.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_artem_galustan.lesson7.homework;
+
+public class ArrayLength3 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers.length);
+    }
+
+}
+
+

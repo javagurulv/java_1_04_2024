7dd9fa252d8a64764647d07f19699cbd9fcf6d5a
Artjoms <galustjans.artjoms@gmail.com>
2024-06-05 20:59:13 +0300

    Lesson 7

---
+++ src/main/java/lv/javaguru/java1/student_artem_galustan/lesson7/homework/ArrayDefinition5.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_artem_galustan.lesson7.homework;
+
+public class ArrayDefinition5 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_artem_galustan/lesson7/homework/ArrayLength1.java
@@ -0,0 +1,12 @@
+package lv.javaguru.java1.student_artem_galustan.lesson7.homework;
+
+public class ArrayLength1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[1];
+        System.out.println(numbers[0]);
+
+    }
+
+}
+
+// В данном коде мы хотели использовать ячейку которой не существует "[-1]" так как мы написали в созании массива [1] это значит что счисление начинается с 0.

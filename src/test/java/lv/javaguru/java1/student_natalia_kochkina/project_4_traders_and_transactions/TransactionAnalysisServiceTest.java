44ff03ff2eda42d1ad9dc754432178d0cce27bfa
Natalia Kochkina <tisha.silence12@gmail.com>
2024-06-05 23:28:55 +0300

    Project4 Traders and transactions steps 3, 4

--- src/test/java/lv/javaguru/java1/student_natalia_kochkina/project_4_traders_and_transactions/TransactionAnalysisServiceTest.java
+++ src/test/java/lv/javaguru/java1/student_natalia_kochkina/project_4_traders_and_transactions/TransactionAnalysisServiceTest.java
@@ -11,27 +11,38 @@ class TransactionAnalysisServiceTest {
     private TransactionAnalysisService service = new TransactionAnalysisService();
 
     @Test
-    void shouldFindTransactionsFor2011Year() {
-        List<Transaction> transactions = service.findTransactionsByYear(
-                TransactionTestData.getTransactions(), 2011
+    void shouldFindTransactionsIn2011Year() {
+        List<Transaction> transactions = service.findTransactionsIn2011Year(
+                TransactionTestData.getTransactions()
         );
         assertEquals(transactions.size(), 2);
     }
 
     @Test
-    void shouldFindTransactionsFor2012Year() {
-        List<Transaction> transactions = service.findTransactionsByYear(
+    void shouldFindTransactionsByYear() {
+        List<Transaction> transactions2012 = service.findTransactionsByYear(
                 TransactionTestData.getTransactions(), 2012
         );
-        assertEquals(transactions.size(), 4);
+        assertEquals(transactions2012.size(), 4);
+
+        List<Transaction> transactions2013 = service.findTransactionsByYear(
+                TransactionTestData.getTransactions(), 2013
+        );
+        assertEquals(transactions2013.size(), 0);
     }
 
     @Test
-    void shouldFindTransactionsFor2013Year() {
-        List<Transaction> transactions = service.findTransactionsByYear(
-                TransactionTestData.getTransactions(), 2013
+    void shouldSortTransactionsByValue() {
+        List<Transaction> transactions = service.sortTransactionsByValue(
+                TransactionTestData.getTransactions()
         );
-        assertEquals(transactions.size(), 0);
+        assertEquals(transactions.size(), 6);
+        assertEquals(300, transactions.get(0).getValue());
+        assertEquals(400, transactions.get(1).getValue());
+        assertEquals(700, transactions.get(2).getValue());
+        assertEquals(710, transactions.get(3).getValue());
+        assertEquals(950, transactions.get(4).getValue());
+        assertEquals(1000, transactions.get(5).getValue());
     }
 
 }

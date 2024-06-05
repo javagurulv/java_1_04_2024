ed8c69caef35866438ad357dcb034b0f421ac763
Natalia Kochkina <tisha.silence12@gmail.com>
2024-06-05 23:11:03 +0300

    Fix errors

--- src/main/java/lv/javaguru/java1/student_jelena_k/lesson3/DogApp.java
+++ src/main/java/lv/javaguru/java1/student_jelena_k/lesson3/DogApp.java
@@ -1,11 +1,11 @@
 package lv.javaguru.java1.student_jelena_k.lesson3;
 
 class DogApp {
-    public static void main(String[] args) {
-        Dog dog1 = new Dog("Bim", 3);
-        System.out.println(dog1.sayHello());
-        System.out.println(dog1.sayAge());
-        dog1.happyBirthday();
-        System.out.println(dog1.sayAge());
-    }
+//    public static void main(String[] args) {
+//        Dog dog1 = new Dog("Bim", 3);
+//        System.out.println(dog1.sayHello());
+//        System.out.println(dog1.sayAge());
+//        dog1.happyBirthday();
+//        System.out.println(dog1.sayAge());
+//    }
 }

096bf9eb28c4556726bfd565c2c197b4d2a2be8f
NSnadezhda <nadezda.senko@gmail.com>
2024-06-07 19:41:00 +0300

    lesson7 level1

---
+++ src/main/java/lv/javaguru/java1/student_nadezda_radigina/lesson_7/level_1_intern/ArrayLength1.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_nadezda_radigina.lesson_7.level_1_intern;
+
+ class ArrayLength1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[1];
+        System.out.println(numbers[0]);
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_nadezda_radigina/lesson_7/level_1_intern/ArrayLength2.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_nadezda_radigina.lesson_7.level_1_intern;
+
+ class ArrayLength2 {
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+
+    }
+}

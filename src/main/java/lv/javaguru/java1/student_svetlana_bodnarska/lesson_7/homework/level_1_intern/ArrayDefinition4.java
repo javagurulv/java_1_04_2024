2838b2cce012def7a241585f71c678c0b9ef7cf5
S286B826 <svetlana.bodnarska@inbox.lv>
2024-06-08 17:30:29 +0300

    lesson_7_level_1

---
+++ src/main/java/lv/javaguru/java1/student_svetlana_bodnarska/lesson_7/homework/level_1_intern/ArrayDefinition4.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_svetlana_bodnarska.lesson_7.homework.level_1_intern;
+
+class ArrayDefinition4
+{
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+    }
+}
---
+++ src/main/java/lv/javaguru/java1/student_svetlana_bodnarska/lesson_7/homework/level_1_intern/ArrayLength1.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_svetlana_bodnarska.lesson_7.homework.level_1_intern;
+
+class ArrayLength1
+{
+    public static void main(String[] args) {
+        int[] numbers = new int[1];
+        System.out.println(numbers[0]);
+    }
+}

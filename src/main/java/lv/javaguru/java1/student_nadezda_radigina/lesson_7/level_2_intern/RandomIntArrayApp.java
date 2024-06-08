cd0005fee2bdf0814571c11ba22cbf5cd3626d91
NSnadezhda <nadezda.senko@gmail.com>
2024-06-08 14:04:57 +0300

    lesson7 level2

---
+++ src/main/java/lv/javaguru/java1/student_nadezda_radigina/lesson_7/level_2_intern/RandomIntArrayApp.java
@@ -0,0 +1,10 @@
+package lv.javaguru.java1.student_nadezda_radigina.lesson_7.level_2_intern;
+
+ class RandomIntArrayApp {
+
+     public static void main(String[] args){
+         RandomIntArray randomIntArray = new RandomIntArray();
+         int[] randomArray = randomIntArray.valuesRandom();
+         randomIntArray.randomValues(randomArray);
+     }
+}

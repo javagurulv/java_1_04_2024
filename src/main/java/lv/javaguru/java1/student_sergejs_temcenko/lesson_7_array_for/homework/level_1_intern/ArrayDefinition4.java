3d981ce34b308c7fbe51ea1709b4f0056cb93dc9
stemcenko <67960125+stemcenko@users.noreply.github.com>
2024-06-08 08:48:06 +0300

    Lesson_7 homework level_1_intern

---
+++ src/main/java/lv/javaguru/java1/student_sergejs_temcenko/lesson_7_array_for/homework/level_1_intern/ArrayDefinition4.java
@@ -0,0 +1,14 @@
+package lv.javaguru.java1.student_sergejs_temcenko.lesson_7_array_for.homework.level_1_intern;
+
+class ArrayDefinition4 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+
+        System.out.println(numbers.length);
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+    }
+
+}
---
+++ src/main/java/lv/javaguru/java1/student_sergejs_temcenko/lesson_7_array_for/homework/level_1_intern/ArrayLength2.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java1.student_sergejs_temcenko.lesson_7_array_for.homework.level_1_intern;
+
+class ArrayLength2 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+    }
+
+}

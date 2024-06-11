68eafe68587d940302e464f80c4f39969c6e789a
Ujin <troi_daniel@mail.ru>
2024-06-11 20:08:30 +0300

    ReBuild task

---
+++ src/main/java/lv/javaguru/java1/student_eyheni_salmin/lesson_2/Class/Calculator.java
@@ -0,0 +1,28 @@
+package lv.javaguru.java1.student_eyheni_salmin.lesson_2.Class;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        printResult(100, 200);
+
+    }
+    public static int getNumSumm(int NumOne, int NumTwo) {
+        return NumOne + NumTwo;
+    }
+    public static int getNumSubstr(int NumOne, int NumTwo) {
+        return NumOne - NumTwo;
+    }
+    public static int getNumMultipl(int NumOne, int NumTwo) {
+        return NumOne * NumTwo;
+    }
+    public static int getNumDivision(int NumOne, int NumTwo) {
+        return NumOne / NumTwo;
+    }
+
+    public static void printResult(int NumOne, int NumTwo) {
+        System.out.println("Сумма: " + getNumSumm(NumOne,NumTwo));
+        System.out.println("Разность: " + getNumSubstr(NumOne,NumTwo));
+        System.out.println("Произведение: " + getNumMultipl(NumOne,NumTwo));
+        System.out.println("Частное: " + getNumDivision(NumOne,NumTwo));
+    }
+}

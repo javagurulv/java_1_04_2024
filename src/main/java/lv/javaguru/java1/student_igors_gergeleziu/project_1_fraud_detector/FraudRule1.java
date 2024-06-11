2a1bc18ed7328162081ca93e7034f04e9722ef38
MrJohnyRony <disqueeze@mail.ru>
2024-06-11 05:21:55 +0300

    project_1_fraud_detector step_13

---
+++ src/main/java/lv/javaguru/java1/student_igors_gergeleziu/project_1_fraud_detector/FraudRule1.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.student_igors_gergeleziu.project_1_fraud_detector;
+
+class FraudRule1 implements FraudRule {
+    @Override
+    public boolean isFraud(Transaction transaction) {
+        Trader trader = transaction.getTrader();
+        return trader.getFullName().equals("Pokemon");
+    }
+}

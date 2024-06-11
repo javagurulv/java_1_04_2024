b01b5398d110ff7bfc3b975325b8503c549bc9d8
Ujin <troi_daniel@mail.ru>
2024-06-11 14:10:20 +0300

    Build task

---
+++ src/main/java/lv/javaguru/java1/student_eyheni_salmin/lesson_1/Summator.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_eyheni_salmin.lesson_1;
+
+public class Summator {
+    public static void main(String[] args) {
+        printSumm();
+    }
+    public static int getSumm() {
+        return Integer.sum(74, 36);
+    }
+    public static void printSumm() {
+        System.out.println(getSumm());
+    }
+}

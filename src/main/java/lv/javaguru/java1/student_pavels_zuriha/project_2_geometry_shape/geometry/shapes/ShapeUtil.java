3f1fc5555c97d33e96a208c1aab504b3165092da
Pahanec <87240194+Pahanec@users.noreply.github.com>
2024-06-12 23:14:57 +0300

    Project 2 Geometry Shape Step 7,8,9,10

---
+++ src/main/java/lv/javaguru/java1/student_pavels_zuriha/project_2_geometry_shape/geometry/shapes/ShapeUtil.java
@@ -0,0 +1,37 @@
+package lv.javaguru.java1.student_pavels_zuriha.project_2_geometry_shape.geometry.shapes;
+
+import java.util.Random;
+
+class ShapeUtil {
+
+    private Random randomNumbers = new Random();
+
+    public Circle createRandomCircle() {
+        double radius = randomNumbers.nextDouble((6) + 1);
+        return new Circle("Circle",radius);
+    }
+
+    public Square createRandomSquare() {
+        double length = randomNumbers.nextDouble((6) +1);
+        return new Square("Square", length);
+    }
+
+    public Rectangle createRandomRectangle() {
+        double length = randomNumbers.nextDouble((6) + 1);
+        double width = randomNumbers.nextDouble((6) + 1);
+        return new Rectangle("Rectangle", length, width);
+    }
+
+    public Shape createRandomShape() {
+
+        double random = randomNumbers.nextInt(2);
+
+        if (random == 0) {
+            return createRandomCircle();
+        } else if (random == 1) {
+            return createRandomSquare();
+        } else {
+            return createRandomRectangle();
+        }
+    }
+}

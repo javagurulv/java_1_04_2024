6d83b548fb3ec333f6f69fb2b731631d46816472
Natalia Kochkina <tisha.silence12@gmail.com>
2024-06-18 22:51:28 +0300

    Lesson 8 day3

---
+++ src/main/java/lv/javaguru/java1/student_natalia_kochkina/lesson_8/homework/day_3/Car.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_natalia_kochkina.lesson_8.homework.day_3;
+
+class Car {
+
+    private String model;
+    private String color;
+
+    public Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+}

44ac36481999e5cfbd67034b7250edce6abda0b1
TheMaksimys <maksims.latkovskis@gmail.com>
2024-06-19 11:29:21 +0300

    Attempted to Implement Step 15

--- src/main/java/lv/javaguru/java1/student_maksims_latkovskis/project_1_fraud_detector/FraudDetector.java
+++ src/main/java/lv/javaguru/java1/student_maksims_latkovskis/project_1_fraud_detector/FraudDetector.java
@@ -22,7 +22,22 @@ class FraudDetector {
                 return rule.get();
             }
         }
-        return new FraudDetectionResult(false, null);
+           return new FraudDetectionResult(false, null);
 
+         /*  IT DOESN'T WORK ANYWAY....
+        return fraudRules.stream()
+                .filter(fraudRule -> fraudRule.get().isFraud())
+                .findFirst()
+                .map(this::buildFraudResult)
+                .orElse(buildNotFraudResult());
+          */
     }
+
+    private FraudDetectionResult buildNotFraudResult() {
+        return new FraudDetectionResult(false, null);
+    }
+    private FraudDetectionResult buildFraudResult(FraudRule fraudRule) {
+        return new FraudDetectionResult(true, fraudRule.getRuleName());
+    }
+
 }

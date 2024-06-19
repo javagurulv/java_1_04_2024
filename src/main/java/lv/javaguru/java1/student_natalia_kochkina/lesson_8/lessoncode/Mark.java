692633502818ec959cd92321dc2e85db1631f7c4
Natalia Kochkina <tisha.silence12@gmail.com>
2024-06-19 23:35:28 +0300

    Lesson 8 lessoncode

---
+++ src/main/java/lv/javaguru/java1/student_natalia_kochkina/lesson_8/lessoncode/Mark.java
@@ -0,0 +1,21 @@
+package lv.javaguru.java1.student_natalia_kochkina.lesson_8.lessoncode;
+
+class Mark {
+
+    private String subject;
+    private int mark;
+
+    public Mark(String subject, int mark) {
+        this.subject = subject;
+        this.mark = mark;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+}

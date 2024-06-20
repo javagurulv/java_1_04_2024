08d4a1815ceb8a0bc92f8e233e70f707e2e0b253
Den <deniss.boltunovs@gmail.com>
2024-06-20 19:12:12 +0300

    Lesson 8 homework day_2

--- src/main/java/lv/javaguru/java1/student_deniss_boltunovs/lesson_8/homework/day_2/IntegerList.java
+++ src/main/java/lv/javaguru/java1/student_deniss_boltunovs/lesson_8/homework/day_2/IntegerList.java
@@ -9,7 +9,7 @@ class IntegerList {
    private List<Integer> list;
 
     IntegerList() {
-        List<Integer> numbers = new ArrayList<Integer>();
+        List<Integer> numbers = new ArrayList<>();
         numbers.addAll(Arrays.asList(1,2,3,4,6));
         this.list = numbers;
     }

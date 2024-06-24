0bac085051a0993500e94d837d51869c551eba46
svetlana <svetlana.bodnarska@inbox.lb>
2024-06-24 17:27:13 +0300

    Lesson 5_Level_4

---
+++ src/main/java/lv/javaguru/java1/student_svetlana_bodnarska/lesson_5/homework/level_4_junior/CircleAreaAndPerimeter.java
@@ -0,0 +1,15 @@
+package lv.javaguru.java1.student_svetlana_bodnarska.lesson_5.homework.level_4_junior;
+
+class CircleAreaAndPerimeter
+{
+   double radius;
+
+   double circlePerimeter (double radius){
+       return 2 * 3.141592654 * radius;
+   }
+
+   double circleArea (double radius){
+       return 3.141592654 * radius * radius;
+   }
+}
+
---
+++ src/main/java/lv/javaguru/java1/student_svetlana_bodnarska/lesson_5/homework/level_4_junior/CircleAreaAndPerimeterApp.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_svetlana_bodnarska.lesson_5.homework.level_4_junior;
+
+class CircleAreaAndPerimeterApp
+{
+    public static void main(String[] args) {
+
+        CircleAreaAndPerimeter circleAreaAndPerimeter = new CircleAreaAndPerimeter();
+
+        System.out.println("Circle perimeter = " + circleAreaAndPerimeter.circlePerimeter(30));
+        System.out.println("Circle area = " + circleAreaAndPerimeter.circleArea(30));
+
+    }
+}

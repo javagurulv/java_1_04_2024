ba6277e4169988c92c1ae14685c0d4b7639e3a1c
Ujin <troi_daniel@mail.ru>
2024-06-27 22:08:02 +0300

    ReBuild task

---
+++ src/main/java/lv/javaguru/java1/student_eyheni_salmin/lesson_4/class_work/RangeNumbers.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java1.student_eyheni_salmin.lesson_4.class_work;
+
+import java.util.Scanner;
+
+public class RangeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        if ((number >= 1) && (number <= 10)) System.out.println("Number in range");
+        else System.out.println("Is not");
+    }
+}
